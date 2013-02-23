2d89c9d2441101fc79ae8a70bbebb72e198613eb
Alexio <alexioamota@gmail.com>
2013-02-23 16:46:16 -0500

    Side Nav Updates

--- includes/side_nav.php
+++ includes/side_nav.php
@@ -1,7 +1,41 @@
-<div class="three pull-nine mobile-four columns">
-<div class="row">
-	
-    <p>Side navigation bar</p>
+<div class="three pull-nine mobile-four columns sidenav_custom">
+	<br>
+	<!--Donate-->
+	<div class="row">
+		<a class="button-resize small round button" href="donate.php">Make a Promise</a>
+	</div>
 
-</div>
+	<hr>
+	<!--View/Create Groups-->
+	<div class="row">
+		<a class="button-resize small round button" href="group.php">View/Create a Group</a>
+	</div>
+
+	<hr>
+	<!--Country Selection -->
+	<div class="row">
+		<div href="#" class="button-resize small round button dropdown">
+			Choose a Country
+			<ul>
+		   		<!-- php queries for country -->
+		   	</ul>
+		</div>
+	</div>	
+
+	<br> 
+	<!--School Selection-->
+	<div class="row">
+		<div href="#" class="button-resize small round button dropdown">
+			Choose a Project
+			<ul>
+		   		<!-- php queries for country -->
+		   	</ul>
+		</div>
+	</div>
+
+	<hr>
+	<!--FACEBOOK INTEGRATION -->
+
+	<div class="row">
+	</div>
 </div>
