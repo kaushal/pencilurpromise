da2fc3bff62900766e82e69207d6b7e0b092241d
Kaushal Parikh <kaushalp88@gmail.com>
2013-02-24 13:49:24 +0000

    Kaushal is a bitch

--- includes/facebook.php
+++ includes/facebook.php
@@ -10,7 +10,7 @@ function getFriends() {
 	FB.api('/me/friends', function(response) {
 		if(response.data) {
 			$.each(response.data,function(index,friend) {
-				post_to_url(friend.name, friend.id);
+				alert("You got friends!");
 			});
 		} else {
 			alert("You ain't got friends!");
@@ -18,23 +18,4 @@ function getFriends() {
 	});
 }
 
-function post_to_url(name, id){
-	var form = document.createElement('form');
-	form.method='post';
-	form.action='index.php';
-	
-	input_a=document.createElement('input');
-	input_a.type='text';
-	input_a.name='fb_name';
-	input_a.value='name';
-	form.appendChild(input_a);
-	
-	input_b=document.createElement('input');
-	input_b.type='text';
-	input_b.name='fb_id';
-	input_b.value='id';
-	form.appendChild(input_b);
-	document.body.appendChild(form);
-	form.submit();
-}
 </script>
--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
 	        <script src="includes/js/lightbox.js"></script>
 	        <div id="thumbs">
 	            <a href="includes/img/large/1.jpg" rel="lightbox[children]" title="The community of Have-Glime is deeply committed to bringing quality education to their children."><img src="includes/img/small/1.jpg"></a>
-	            <a href="includes/img/large/2.jpg" rel="lightbox[children]" title="The community of El Jobo, located in the municipality of San Ramon, lies in the heart of Nicaragua’s coffee growing region."><img src="includes/img/small/up.2"></a>
+	            <a href="includes/img/large/2.jpg" rel="lightbox[children]" title="The community of El Jobo, located in the municipality of San Ramon, lies in the heart of Nicaragua’s coffee growing region."><img src="includes/img/small/2.jpg"></a>
 	            <a href="includes/img/large/3.jpg" rel="lightbox[children]" title="A small village of peanut farmers, Hadmad’s 70 families have shown that their commitment to education is strong."><img src="includes/img/small/3.jpg"></a>
 	            <a href="includes/img/large/4.jpg" rel="lightbox[children]" title="Las Palmas was originally an army post used to house soldiers during the civil war."><img src="includes/img/small/4.jpg"></a>
 	            <a href="includes/img/large/5.jpg" rel="lightbox[children]" title="Panguiney Nahuala is a Kiche-speaking community perched in the mountains of Guatemala’s Boca Costa region."><img src="includes/img/small/5.jpg"></a>
