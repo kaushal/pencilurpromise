5037a1c967c9d3a7a14783f965c74aba65fa1e42
Hua Yang <huacyang@gmail.com>
2013-02-24 10:33:56 -0500

    134

--- includes/facebook.php
+++ includes/facebook.php
@@ -10,19 +10,16 @@ function getFriends() {
 	FB.api('/me/friends', function(response) {
 		if(response.data) {
 			$.each(response.data,function(index,friend) {
-				var div1 = document.createElement("div");
-				var div2 = document.createElement("div");
+				var div = document.createElement("div");
 				var check = document.createElement("input");
 				var face = document.createElement("p");
 				face.innerHTML = friend['name'];
 				check.type = "checkbox";
 				check.name = friend['name'];
-				div1.className = "eleven columns";
-				div1.appendChild(face);
-				div2.className = "one column";
-				div2.appendChild(check);
-				document.getElementById("fb").appendChild(div1);
-				document.getElementById("fb").appendChild(div2);
+				div.className = "row";
+				div.appendChild(face);
+				div.appendChild(check);
+				document.getElementById("fb").appendChild(div);
 			});
 		} else {
 			alert("You ain't got friends!");
