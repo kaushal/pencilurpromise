f7363ef93fe91fadad9914646f3a02cba4346abe
Alexio <alexioamota@gmail.com>
2013-02-24 02:42:33 -0500

    Footer update

--- includes/footer.php
+++ includes/footer.php
@@ -1,24 +1,25 @@
 <footer class="row">
     <div class="twelve columns"><hr />
     <div class="row">
-    	
-        <div class="twelve columns">
-        <script type=text/javascript src="{{ url_for('static', filename='jquery.js') }}"></script>
-        <script src="http://code.jquery.com/jquery-1.8.2.min.js" type="text/javascript"></script>
-        <script src="js/lightbox.js"></script>
-        <link href="css/lightbox.css" rel="stylesheet" />
-        <div id="thumbs">
-            <a href="img/large/toystory.jpg" rel="lightbox[children]" title="This is Toy Story!"><img src="img/small/toystory.jpg"></a>
-            <a href="img/large/up.jpg" rel="lightbox[children]" title="This is up!"><img src="img/small/up.jpg"></a>
-            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
-            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
-            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
-        </div>
-            <p class=copy>Copyright &copy; Code 4 Good, 2013</p>
 
-        <div class="twelve columns text-center">
-            <p class=copy>Copyright &copy; 2013, Code 4 Good</p>
-        </div>
+        <div class="twelve columns">
+	        <script type=text/javascript src="{{ url_for('static', filename='jquery.js') }}"></script>
+	        <script src="http://code.jquery.com/jquery-1.8.2.min.js" type="text/javascript"></script>
+	        <script src="js/lightbox.js"></script>
+	        <link href="css/lightbox.css" rel="stylesheet" />
+	        <div id="thumbs">
+	            <a href="img/large/toystory.jpg" rel="lightbox[children]" title="This is Toy Story!"><img src="img/small/toystory.jpg"></a>
+	            <a href="img/large/up.jpg" rel="lightbox[children]" title="This is up!"><img src="img/small/up.jpg"></a>
+	            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
+	            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
+	            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
+	        </div>
+	            <p class=copy>Copyright &copy; Code 4 Good, 2013</p>
+
+	        <div class="twelve columns text-center">
+	            <p class=copy>Copyright &copy; 2013, Code 4 Good</p>
+	        </div>
+    	</div>
     </div>
     </div>
 </footer>
