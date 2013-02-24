b72efc73946c9e9f68c73eb08d06f63d95efa712
Sam Agnew <sam@sam-Studio-1555.(none)>
2013-02-24 08:55:44 -0500

    All the children

--- includes/footer.php
+++ includes/footer.php
@@ -6,11 +6,11 @@
 	        <script src="http://code.jquery.com/jquery-1.8.2.min.js" type="text/javascript"></script>
 	        <script src="includes/js/lightbox.js"></script>
 	        <div id="thumbs">
-	            <a href="includes/img/large/1.jpg" rel="lightbox[children]" title="The community of Have-Glime is deeply committed to bringing quality education to their children."><img src="includes/img/small/1.jpg"></a>
-	            <a href="includes/img/large/2.jpg" rel="lightbox[children]" title="The community of El Jobo, located in the municipality of San Ramon, lies in the heart of Nicaragua’s coffee growing region."><img src="includes/img/small/2.jpg"></a>
-	            <a href="includes/img/large/3.jpg" rel="lightbox[children]" title="A small village of peanut farmers, Hadmad’s 70 families have shown that their commitment to education is strong."><img src="includes/img/small/3.jpg"></a>
-	            <a href="includes/img/large/4.jpg" rel="lightbox[children]" title="Las Palmas was originally an army post used to house soldiers during the civil war."><img src="includes/img/small/4.jpg"></a>
-	            <a href="includes/img/large/5.jpg" rel="lightbox[children]" title="Panguiney Nahuala is a Kiche-speaking community perched in the mountains of Guatemala’s Boca Costa region."><img src="includes/img/small/5.jpg"></a>
+	            <a href="includes/img/large/1.jpg" rel="lightbox[children]" title="The community of Have-Glime is deeply committed to bringing quality education to their children."><img width="100" height="100" src="includes/img/small/1.jpg"></a>
+	            <a href="includes/img/large/2.jpg" rel="lightbox[children]" title="The community of El Jobo, located in the municipality of San Ramon, lies in the heart of Nicaragua’s coffee growing region."><img width="100" height="100" src="includes/img/small/2.jpg"></a>
+	            <a href="includes/img/large/3.jpg" rel="lightbox[children]" title="A small village of peanut farmers, Hadmad’s 70 families have shown that their commitment to education is strong."><img width="100" height="100" src="includes/img/small/3.jpg"></a>
+	            <a href="includes/img/large/4.jpg" rel="lightbox[children]" title="Las Palmas was originally an army post used to house soldiers during the civil war."><img width="100" height="100" src="includes/img/small/4.jpg"></a>
+	            <a href="includes/img/large/5.jpg" rel="lightbox[children]" title="Panguiney Nahuala is a Kiche-speaking community perched in the mountains of Guatemala’s Boca Costa region."><img width="100" height="100" src="includes/img/small/5.jpg"></a>
 	        </div>
 			<hr />
 	        <div class="twelve columns text-center">
