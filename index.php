48828fc425e1dfcc6f1991a1acd5533c06c2562e
Hua Yang <huacyang@gmail.com>
2013-02-23 23:18:52 -0500

    index: Image selection 90%

--- index.php
+++ index.php
@@ -34,8 +34,6 @@
 <body>
 	<div class="row">
 	<div class="twelve columns" >
-        <!-- Includes Header -->	
-        <?php include("includes/header.php") ?>
         <!-- Includes Navigation -->
         <?php include("includes/nav.php") ?>
     </div>
@@ -43,25 +41,25 @@
     
     <!-- Main Content
     ====================== -->
-    <div class="twelve columns">
+    <div class="twelve columns shift-up">
     <div class="row">
     	<!-- Main Display Area -->
         <div class="nine push-three mobile-four columns">
         <!-- Status bar -->
         <div class="row">
-            <div class="nine columns">
+            <div class="eight columns">
             <div class="row">
-            	<div class="nine columns"><div class="row">
+            	<div class="ten columns"><div class="row">
                 <img src="includes/img/heart-meter.jpg"/>
                 </div></div>
-                <div class="three columns"><div class="row">
+                <div class="two columns"><div class="row">
                 <img src="includes/img/pencil-mini.jpg"/>
                 </div></div>
             </div>
             </div>
-            <div class="three columns">
+            <div class="four columns text-center"><div class="row">
             	<h4>Donation: $1</h4>
-            </div>
+            </div></div>
             <hr/>
         </div>
         <!-- Image -->
@@ -70,28 +68,28 @@
                 <a id="scene_trigger" class="round button" href="javascript:scene_trigger()">to School View</a>
             </div>
             
-            <div class="twelve columns"><div id="blackboard" class="eight columns offset-by-four">
-                <img src="http://placehold.it/200x100"/>
+            <div class="twelve columns"><div id="blackboard" class="six columns end">
+                <img src="includes/img/blackboard.jpg"/>
             </div></div>
             
-            <div class="twelve columns">
-                <div id="chalks" class="six columns"><img src="http://placehold.it/200x100"/></div>
-                <div id="b_eraser"class="six columns"><img src="http://placehold.it/200x100"/></div>
+            <div class="twelve columns shift-down">
+                <div id="chalks" class="three columns offset-by-four"><img src="includes/img/chalk.jpg"/></div>
+                <div id="b_eraser"class="three columns end"><img src="includes/img/b_eraser.jpg"/></div>
             </div>
             
-            <div class="twelve columns"><div id="table" class="eight columns offset-by-four">
-                <img src="http://placehold.it/200x100"/>
+            <div class="twelve columns shift-down"><div id="table" class="eight columns offset-by-four">
+                <img src="includes/img/table.jpg"/>
             </div></div>
             
-            <div class="twelve columns">
-                <div id="eraser" class="four columns"><img src="http://placehold.it/200x100"/></div>
-                <div id="pencil" class="four columns"><img src="http://placehold.it/200x100"/></div>
-                <div id="paper" class="four columns"><img src="http://placehold.it/200x100"/></div>
+            <div class="twelve columns shift-down">
+                <div id="eraser" class="four columns offset-by-one"><img src="includes/img/eraser.jpg"/></div>
+                <div id="pencil" class="three columns"><img src="includes/img/pencil.jpg"/></div>
+                <div id="paper" class="three columns offset-by-one"><img src="includes/img/paper.jpg"/></div>
             </div>
             
-            <div class="twelve columns">
-                <div id="chair" class="six columns"><img src="http://placehold.it/200x100"/></div>
-                <div id="desk" class="six columns"><img src="http://placehold.it/200x100"/></div>
+            <div class="twelve columns shift-down">
+                <div id="chair" class="five columns offset-by-two"><img src="includes/img/chair.jpg"/></div>
+                <div id="desk" class="five columns"><img src="includes/img/desk.jpg"/></div>
             </div>
         </div>
     </div>
