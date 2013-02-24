e50510b03945f058c73c3e23210d0f622c6a49a2
Alexio <alexioamota@gmail.com>
2013-02-24 01:48:45 -0500

    Fb check

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 	$con = mysql_connect("localhost", "root") or die(mysql_error());
 	mysql_select_db("POPDB", $con) or die(mysql_error());
     
-    /*if(isset($_POST['fb_name']) && isset($_POST['fb_id'])){
+    if(isset($_POST['fb_name']) && isset($_POST['fb_id'])){
         $fb_name = $_POST['fb_name'];
         $fb_id = $_POST['fb_id'];
         mysql_query("INSERT INTO Trans (Pname, FBID)
         VALUES('$fb_name', '$fb_id')");
-    }*/
+    }
 ?>
 <!DOCTYPE HTML>
 <html>
