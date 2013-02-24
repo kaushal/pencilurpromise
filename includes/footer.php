c839a3ab06ff0ea1c49d0594f225266757108fff
Sam Agnew <sam@sam-Studio-1555.(none)>
2013-02-24 02:29:41 -0500

    Footer.php should work now

--- includes/footer.php
+++ includes/footer.php
@@ -2,6 +2,17 @@
     <div class="twelve columns"><hr />
     <div class="row">
         <div class="twelve columns">
+        <script type=text/javascript src="{{ url_for('static', filename='jquery.js') }}"></script>
+        <script src="http://code.jquery.com/jquery-1.8.2.min.js" type="text/javascript"></script>
+        <script src="js/lightbox.js"></script>
+        <link href="css/lightbox.css" rel="stylesheet" />
+        <div id="thumbs">
+            <a href="img/large/toystory.jpg" rel="lightbox[children]" title="This is Toy Story!"><img src="img/small/toystory.jpg"></a>
+            <a href="img/large/up.jpg" rel="lightbox[children]" title="This is up!"><img src="img/small/up.jpg"></a>
+            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
+            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
+            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
+        </div>
             <p class=copy>Copyright &copy; Code 4 Good, 2013</p>
         </div>
     </div>
