9ef09cabd615949af5245ff07370dc1122035f56
Hua Yang <huacyang@gmail.com>
2013-02-24 10:42:50 -0500

    139

--- includes/facebook.php
+++ includes/facebook.php
@@ -14,6 +14,7 @@ function getFriends() {
 				var check = document.createElement("input");
 				var face = document.createElement("p");
 				face.innerHTML = friend['name'];
+				face.className = "left";
 				check.type = "checkbox";
 				check.name = friend['name'];
 				check.className = "right";
