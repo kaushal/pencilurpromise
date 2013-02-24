d5c132f77ecef6eb0c40f3b5b92e9596a43d8edc
Sam Agnew <sam@sam-Studio-1555.(none)>
2013-02-24 08:45:42 -0500

    Added the childrens in the pictures

--- includes/footer.php
+++ includes/footer.php
@@ -6,11 +6,11 @@
 	        <script src="http://code.jquery.com/jquery-1.8.2.min.js" type="text/javascript"></script>
 	        <script src="includes/js/lightbox.js"></script>
 	        <div id="thumbs">
-	            <a href="includes/img/large/toystory.jpg" rel="lightbox[children]" title="This is Toy Story!"><img src="includes/img/small/toystory.jpg"></a>
-	            <a href="includes/img/large/up.jpg" rel="lightbox[children]" title="This is up!"><img src="includes/img/small/up.jpg"></a>
-	            <a href="includes/img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="includes/img/small/bugs.jpg"></a>
-	            <a href="includes/img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="includes/img/small/bugs.jpg"></a>
-	            <a href="includes/img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="includes/img/small/bugs.jpg"></a>
+	            <a href="includes/img/large/1.jpg" rel="lightbox[children]" title="The community of Have-Glime is deeply committed to bringing quality education to their children."><img src="includes/img/small/1.jpg"></a>
+	            <a href="includes/img/large/2.jpg" rel="lightbox[children]" title="The community of El Jobo, located in the municipality of San Ramon, lies in the heart of Nicaragua’s coffee growing region."><img src="includes/img/small/up.2"></a>
+	            <a href="includes/img/large/3.jpg" rel="lightbox[children]" title="A small village of peanut farmers, Hadmad’s 70 families have shown that their commitment to education is strong."><img src="includes/img/small/3.jpg"></a>
+	            <a href="includes/img/large/4.jpg" rel="lightbox[children]" title="Las Palmas was originally an army post used to house soldiers during the civil war."><img src="includes/img/small/4.jpg"></a>
+	            <a href="includes/img/large/5.jpg" rel="lightbox[children]" title="Panguiney Nahuala is a Kiche-speaking community perched in the mountains of Guatemala’s Boca Costa region."><img src="includes/img/small/5.jpg"></a>
 	        </div>
 			<hr />
 	        <div class="twelve columns text-center">
