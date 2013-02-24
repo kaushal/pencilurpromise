2575755732305607665e433817e431bd1d3fca8d
Alexio <alexioamota@gmail.com>
2013-02-24 02:45:42 -0500

    SAM I LOVE YOU

--- includes/footer.php
+++ includes/footer.php
@@ -8,13 +8,12 @@
 	        <script src="js/lightbox.js"></script>
 	        <link href="css/lightbox.css" rel="stylesheet" />
 	        <div id="thumbs">
-	            <a href="img/large/toystory.jpg" rel="lightbox[children]" title="This is Toy Story!"><img src="img/small/toystory.jpg"></a>
-	            <a href="img/large/up.jpg" rel="lightbox[children]" title="This is up!"><img src="img/small/up.jpg"></a>
-	            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
-	            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
-	            <a href="img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
+	            <a href="../img/large/toystory.jpg" rel="lightbox[children]" title="This is Toy Story!"><img src="img/small/toystory.jpg"></a>
+	            <a href="../img/large/up.jpg" rel="lightbox[children]" title="This is up!"><img src="img/small/up.jpg"></a>
+	            <a href="../img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
+	            <a href="../img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
+	            <a href="../img/large/bugs.jpg" rel="lightbox[children]" title="This is bugs!"><img src="img/small/bugs.jpg"></a>
 	        </div>
-	            <p class=copy>Copyright &copy; Code 4 Good, 2013</p>
 
 	        <div class="twelve columns text-center">
 	            <p class=copy>Copyright &copy; 2013, Code 4 Good</p>
